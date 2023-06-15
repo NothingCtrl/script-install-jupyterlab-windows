5bd14cdcdd84bd81570f6bb7f655f3624d49c9db
NothingCtrl <nothingctrl@gmail.com>
2023-06-15 15:46:39 +0700

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jupyter LAB
+
+Scripts to install (Jupyter LAB)[https://jupyter.org/install] on Windows OS
+
+* Clone this project, `python`, `pip` and `virtualenv` callable in CMD to continue next step.
+* Run `install_jupyterlab.bat` to install packages
+* After install, run `run_jupyterlab.bat` to launch UI (default port: `8888`)
--- install_jupyterlab.bat
+++ install_jupyterlab.bat
@@ -6,17 +6,21 @@ set DATA_DIR=data
 rem ====================================
 
 if exist %PYTHON_VENV%\ (
+    @echo ==============================
     @echo The "%PYTHON_VENV%" folder is exist! Please delete it to continue install...
+    @echo ==============================
+    pause
 ) else (
     virtualenv %PYTHON_VENV%
     call %PYTHON_VENV%\Scripts\activate.bat
     pip install jupyterlab
     if exist %DATA_DIR%\ (
-        @echo the "%DATA_DIR%" folder is exist!
+        @echo The "%DATA_DIR%" folder is exist!
     ) else (
         mkdir %DATA_DIR%
     )
     @echo ==============================
     @echo Install Jupyter LAB is done! Using script "run_jupyterlab.bat" to start app, all data will save in %DATA_DIR% folder.
     @echo ==============================
+    pause
 )
